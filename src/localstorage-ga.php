9471189a906e8c75dfec7b23f9922bed1416120a
Jorge Blom <bommox@gmail.com>
2016-07-15 14:59:11 +0200

    Uso de action wp_head

--- src/localstorage-ga.php
+++ src/localstorage-ga.php
@@ -13,5 +13,29 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 // No direct access
 defined( 'ABSPATH' ) or die( 'No direct access to this file' );
 
+function inject_local_storage-ga() {
+        $UA_CODE = "UA-XXXXX-Y";
+        echo '<script>
+                window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+                var GA_LOCAL_STORAGE_KEY = "ga:clientId";
+                if (window.localStorage) {
+                    ga("create", "'. $UA_CODE .'", "auto", {
+                        "storage": "none",
+                        "clientId": localStorage.getItem(GA_LOCAL_STORAGE_KEY)
+                    });
+                    ga(function(tracker) {
+                        localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get("clientId"));
+                    });
+                } else {
+                    ga("create", "'. $UA_CODE .'", "auto");
+                }
+                ga("send", "pageview");
+            </script>';
+}
+
+wp_enqueue_script('local-storage-ga');
+
+add_action('wp_head', 'inject_local_storage-ga');
+
 
 ?>
